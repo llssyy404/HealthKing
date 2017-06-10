844807f906ff4e81c2b34d37b37601eab567bd57
llssyy404 <christian404@naver.com>
2017-06-10 18:21:25 +0900

    Info Script추가

---
+++ Student/HealthKing_Student/Assets/Scripts/PAPSInfo.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAPSInfo {
+    private CardiovascularEndurance _cardiovascularEndurance;
+    private Agility                 _agility;
+    private MuscularEndurance       _muscularEndurance;
+    private Flexibility             _flexibility;
+    private BMI                     _BMI;
+}
+
+public class CardiovascularEndurance
+{
+    private int _longRunningCount;
+    private int _longRunningMinute;
+    private int _longRunningSecond;
+
+    public bool InitInfo(int longRunningCount, int longRunningMinute, int longRunningSecond)
+    {
+        _longRunningCount = longRunningCount;
+        _longRunningMinute = longRunningMinute;
+        _longRunningSecond = longRunningSecond;
+
+        return true;
+    }
+}
+
+public class Agility
+{
+    private int _fiftyMRunningSecond;
+    private int _standingBroadJumpCm;
+
+    bool InitInfo(int fiftyMRunningSecond, int standingBroadJumpCm)
+    {
+        _fiftyMRunningSecond = fiftyMRunningSecond;
+        _standingBroadJumpCm = standingBroadJumpCm;
+
+        return true;
+    }
+}
+
+public class MuscularEndurance {
+    private int _pushUpCount;
+    private int _sitUpCount;
+    private int _gripRightKG;
+    private int _gripLeftKG;
+
+    public bool SetInfo(int pushUpCount, int sitUpCount, int gripRightKG, int gripLeftKG)
+    {
+        _pushUpCount = pushUpCount;
+        _sitUpCount = sitUpCount;
+        _gripRightKG = gripRightKG;
+        _gripLeftKG = gripLeftKG;
+
+        return true;
+    }
+}
+
+public class Flexibility
+{
+    private int _sitUpperBodyFrontBendCm;
+
+    public bool SetInfo(int sitUpperBodyFrontBendCm)
+    {
+        _sitUpperBodyFrontBendCm = sitUpperBodyFrontBendCm;
+
+        return true;
+    }
+}
+
+public class BMI
+{
+    private int _height;
+    private int _weight;
+
+    public bool SetInfo(int height, int weight)
+    {
+        _height = height;
+        _weight = weight;
+
+        return true;
+    }
+}
---
+++ Student/HealthKing_Student/Assets/Scripts/UserInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo {
+    private string _schoolName;
+    private int _classGrade;
+    private int _classNum;
+    private int _number;
+    private string _name;
+    private string _gender;
+
+    public bool InitUserInfo(string schoolName, int classGrade, int classNum
+        , int number, string name, string gender)
+    {
+        if (schoolName == null)
+            return false;
+
+        if (classGrade == 0)
+            return false;
+
+        if (classNum == 0)
+            return false;
+
+        if (number == 0)
+            return false;
+
+        if (name == null)
+            return false;
+
+        if (gender == null)
+            return false;
+
+        _schoolName = schoolName;
+        _classGrade = classGrade;
+        _classNum = classNum;
+        _number = number;
+        _name = name;
+        _gender = gender;
+
+        return true;
+    }
+
+    public void Render()
+    {
+    }
+}
