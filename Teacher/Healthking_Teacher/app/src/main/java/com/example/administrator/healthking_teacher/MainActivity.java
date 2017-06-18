b9d0a81415f5f9f1dfff007e97f020a4efaef3e5
Lee Jin-Woo <came1230@naver.com>
2017-06-18 18:17:59 +0900

    RFID 클래스 태그 인식 시작, 종료 기능 추가

--- Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/MainActivity.java
+++ Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.administrator.healthking_teacher;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    Button readButton;
+    Button endButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       boolean isConnect = RFIDDevice.getInstance().set();
+
+        if(isConnect) {
+            Log.d("Debug", "Success");
+        }
+        else
+        {
+            Log.d("Debug", "false");
+        }
+
+
+
+        textview = (TextView) findViewById(R.id.tagView);
+        readButton = (Button) findViewById(R.id.readButton);
+        endButton = (Button) findViewById(R.id.endButton);
+
+
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RFIDDevice.getInstance().beginReadTag();
+            }
+        });
+
+        endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String[] tagList = RFIDDevice.getInstance().getTagList();
+
+                Log.d("Debug", String.valueOf(RFIDDevice.getInstance().getTagList().length));
+
+                StringBuilder sb = new StringBuilder();
+
+                if(tagList != null)
+                {
+                    for(int i=0; i<tagList.length; ++i)
+                    {
+                        sb.append(tagList[i].toString());
+                        sb.append("\n");
+                    }
+                }
+                textview.setText(sb.toString());
+                RFIDDevice.getInstance().finishReadTag();
+            }
+    });
+
+
+
+
+
+
     }
 }
