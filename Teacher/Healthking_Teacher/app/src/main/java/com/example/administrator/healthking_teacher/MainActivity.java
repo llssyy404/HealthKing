576af5677b5b84af237b5c2bca0df984ddc5643e
JungMinCho <monamigreen1@gmail.com>
2017-06-18 18:19:15 +0900

    ui 작업중

--- Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/MainActivity.java
+++ Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.administrator.healthking_teacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SettingClick(View v){
+        TextView text = (TextView)findViewById(R.id.ResultText);
+
+        text.setText("설정 버튼을 눌렀습니다.");
+    }
+
+    public void AddStudent(View v){
+        TextView text = (TextView)findViewById(R.id.ResultText);
+
+        text.setText("학생등록 버튼을 눌렀습니다.");
+    }
+
+    public void WagleClick(View v){
+
+        TextView text = (TextView)findViewById(R.id.ResultText);
+
+        text.setText("와글와글 버튼을 눌렀습니다.");
+
+    }
+    public void MissionClick(View v){
+        TextView text = (TextView)findViewById(R.id.ResultText);
+        text.setText("미션 버튼을 눌렀습니다.");
     }
 }
