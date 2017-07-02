2b736162cce90de5ecc5c4f1787b0e2ddad04d5b
Lee Jin-Woo <came1230@naver.com>
2017-07-02 17:16:21 +0900

    레코드 measure ,match 액티비티 추가

---
+++ Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/Record/RecordMeasureActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.healthking_teacher.Record;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.healthking_teacher.R;
+
+public class RecordMeasureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_measure);
+    }
+}
