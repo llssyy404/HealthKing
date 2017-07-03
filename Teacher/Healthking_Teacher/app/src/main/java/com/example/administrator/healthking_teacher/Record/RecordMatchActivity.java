9e88716ed9a2967a7614c6f5ecc1c3cd0766e263
JungMinCho <monamigreen1@gmail.com>
2017-07-03 16:50:44 +0900

    recordmatch 부분 작성중(임시 리스트 생성, 간단한 버튼으로 임시 리스트 불러오기) 추후작업: 임시리스트와 태그 매치작업.

--- Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/Record/RecordMatchActivity.java
+++ Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/Record/RecordMatchActivity.java
@@ -2,14 +2,68 @@ package com.example.administrator.healthking_teacher.Record;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.administrator.healthking_teacher.Data.DataManager;
+import com.example.administrator.healthking_teacher.Data.StudentRecordData;
+import com.example.administrator.healthking_teacher.Data.TagData;
 import com.example.administrator.healthking_teacher.R;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class RecordMatchActivity extends AppCompatActivity {
+
+    private List<StudentRecordData> studentRecordDataList;
+    private List<TagData> tagDatas;
+
+    private List<StudentRecordData> teststudentRecordDataList;
+
+    //////////////////////////////////
+    private Button letsgoButton;
+    private TextView studentRecordIdTextView;
+
+    /////////////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_match);
+
+        studentRecordDataList = new ArrayList<>();
+        studentRecordDataList.clear();
+
+        letsgoButton = (Button)findViewById(R.id.letsgoButton);
+        studentRecordIdTextView = (TextView)findViewById(R.id.studentRecordidTextView);
+
+        for(int i=0;i<10;++i){
+            Date tempdate = new Date();
+            List<Date> templistdate = new ArrayList<>();
+            templistdate.clear();
+            templistdate.add(tempdate);
+            templistdate.add(tempdate);
+            templistdate.add(tempdate);
+            StudentRecordData temp = new StudentRecordData(Integer.toString(i+ 1 * 33 ),tempdate,100,3,templistdate,tempdate);
+            studentRecordDataList.add(temp);
+        }
+
+        //studentRecordDataList.add(DataManager.getInstance().getStudentRecordDataList().get(0));
+        //studentRecordDataList.get(0);
+
+        letsgoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                studentRecordIdTextView.setText(studentRecordDataList.get(0).getId());
+            }
+        });
+
+    }
+
+    public void Test(){
+
+
     }
 }
