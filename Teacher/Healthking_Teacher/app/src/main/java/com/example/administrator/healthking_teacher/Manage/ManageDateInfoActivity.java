cddfedbd1853dbe8c758435cc26e3b6d15233feb
llssyy404 <christian404@naver.com>
2017-07-04 01:50:14 +0900

    학생별 정보, 날짜별 정보 액티비티 추가

---
+++ Teacher/Healthking_Teacher/app/src/main/java/com/example/administrator/healthking_teacher/Manage/ManageDateInfoActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.healthking_teacher.Manage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.healthking_teacher.R;
+
+/**
+ * Created by seru on 2017-07-04.
+ */
+
+public class ManageDateInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_dateinfo);
+    }
+}
