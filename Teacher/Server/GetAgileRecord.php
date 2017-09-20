cebec1137e8722a35a147fdc3be8de3be5dbbf6c
llssyy404 <christian404@naver.com>
2017-09-20 11:35:12 +0900

    학생앱 로그인한 학생의 순발력 기록 가져오는 php 파일 추가, GetCardiRecord.php 수정

--- Teacher/Server/GetAgileRecord.php
+++ Teacher/Server/GetAgileRecord.php
@@ -2,13 +2,13 @@
 $con = mysqli_connect("localhost","came1230","Healthking1!","came1230");
 
 $StudentID = $_POST["ID"];
-$result = mysqli_query($con, "SELECT * FROM CARDIRECORD WHERE StudentID = '$StudentID'");
+$result = mysqli_query($con, "SELECT * FROM AGILERECORD WHERE StudentID = '$StudentID'");
 
 $response = array();
 
 while ($row = mysqli_fetch_array($result))
 {
-	array_push($response, array("RecordUnique" =>$row[0], "StudentID" =>$row[1], "Date" =>$row[2], "TotalMeter"=>$row[3], "TotalTrackCount"=>$row[4], "TotalElapsedTime"=>$row[5]));
+	array_push($response, array("RecordUnique" =>$row[0], "StudentID" =>$row[1], "Date" =>$row[2], "Meter"=>$row[3], "ElapsedTime"=>$row[4]));
 }
 
 echo json_encode(array("response" => $response), JSON_UNESCAPED_UNICODE);
