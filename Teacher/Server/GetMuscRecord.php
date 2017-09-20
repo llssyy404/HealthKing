7b019df5eee7c61aab5cb65de0985b653a3e2f3e
llssyy404 <christian404@naver.com>
2017-09-20 11:47:39 +0900

    학생앱 로그인한 학생의 근력근지구력 기록 가져오는 php 파일 추가

---
+++ Teacher/Server/GetMuscRecord.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("localhost","came1230","Healthking1!","came1230");
+
+$StudentID = $_POST["ID"];
+$result = mysqli_query($con, "SELECT * FROM MUSCRECORD WHERE StudentID = '$StudentID'");
+
+$response = array();
+
+while ($row = mysqli_fetch_array($result))
+{
+	array_push($response, array("RecordUnique" =>$row[0], "StudentID" =>$row[1], "Date" =>$row[2], "Count"=>$row[3]));
+}
+
+echo json_encode(array("response" => $response), JSON_UNESCAPED_UNICODE);
+mysqli_close($con);
+
+?>
